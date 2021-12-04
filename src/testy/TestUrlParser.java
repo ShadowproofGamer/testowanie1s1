f589a14bd4edeac19b405c83753af873b51796ea
ShadowproofGamer <kubac400@gmail.com>
2021-12-04 17:58:05 +0100

    Initial commit

---
+++ src/testy/TestUrlParser.java
@@ -0,0 +1,15 @@
+package testy;
+
+import labunittest.UrlParser;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+
+public class TestUrlParser {
+    @Test
+    public void testUrlParse(){
+        UrlParser parser = new UrlParser();
+        
+    }
+
+}
